df099345ba665084545531b3892ea8dd4a7a861b
LUCAS\ADMIN <hoanguyendinh0494@gmail.com>
2016-12-07 16:58:55 +0700

    Product Commit 16h58

--- NFCDashboard-server/src/main/java/nfc/controller/SupplierWorkManagementController.java
+++ NFCDashboard-server/src/main/java/nfc/controller/SupplierWorkManagementController.java
@@ -27,24 +27,6 @@ public class SupplierWorkManagementController {
 	@RequestMapping(value="supplierwork/add", method=RequestMethod.POST)
 	public @ResponseBody String insertSupplierWork(@RequestBody SupplierWork supplierwork){
 		//SupplierWork.setApp_id(Utils.appId);
-		supplierwork.setSuppl_id(1);
-		supplierwork.setIs_active(1);
-		supplierwork.setWd_start_hm("08:00");
-		supplierwork.setWd_end_hm("22:00");
-		supplierwork.setSat_start_hm("08:00");
-		supplierwork.setSat_end_hm("16:00");
-		supplierwork.setSun_start_hm("08:00");
-		supplierwork.setSun_end_hm("13:00");
-		supplierwork.setIs_online(1);
-		supplierwork.setRank5(3);
-		supplierwork.setOrder_count(500);
-		supplierwork.setFavorite_count(200);
-		supplierwork.setCall_order(982347);
-		supplierwork.setDirect_pay(78236);
-		supplierwork.setVisit_pay(97823);
-		supplierwork.setMin_order_amt(10);
-		supplierwork.setOwner_suppl_id(0);
-		supplierwork.setManage_suppl_id(0);
 		String data = supplierworkDAO.insertSupplierWork(supplierwork)+"";
 		return "{\"result\":\"" + data + "\"}";
 	}
--- NFCDashboard-server/src/main/java/nfc/serviceImpl/ProductService.java
+++ NFCDashboard-server/src/main/java/nfc/serviceImpl/ProductService.java
@@ -1,16 +1,25 @@
 package nfc.serviceImpl;
+import java.io.Serializable;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.hibernate.Criteria;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
+import org.hibernate.criterion.Restrictions;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import nfc.model.Code;
 import nfc.model.Product;
+import nfc.model.Role;
+import nfc.model.Supplier;
 import nfc.service.IProductService;
+import nfc.service.common.ICommonService;
 public class ProductService implements IProductService {
 	@Autowired
+	private ICommonService commonDAO;
 	private SessionFactory sessionFactory;
 	public void setSessionFactory(SessionFactory sessionFactory) {
 		this.sessionFactory = sessionFactory;
@@ -22,6 +31,39 @@ public class ProductService implements IProductService {
 		List<Product> list = (List<Product>) criteria.list();
 		trans.commit();
 		return list;
-		
+	}
+	public boolean insertProduct(Product product){
+		Session session = this.sessionFactory.getCurrentSession();
+		Transaction trans = session.beginTransaction();
+		try
+		{
+			int roleIdDesc = 0;
+			Serializable ser = session.save(product);
+	        if (ser != null) {
+	        	roleIdDesc = (Integer) ser;
+	        }
+			Map<String, String> map = new HashMap<String, String>();
+			map.put("group_code", "0003");
+			map.put("sub_code", roleIdDesc+"");
+			Code code = (Code) commonDAO.createObject("nfc.model.Code", map);
+			session.save(code);
+			trans.commit();
+			return true;
+		}
+		catch(Exception ex)
+		{
+			System.out.println("Error " + ex.getMessage());
+			trans.rollback();
+			return false;
+		}
+	}
+	public Product getProduct(String prodId){
+		Session session = this.sessionFactory.getCurrentSession();
+		Transaction trans = session.beginTransaction();
+		Criteria criteria = session.createCriteria(Product.class);
+		criteria.add(Restrictions.eq("prod_id", Integer.parseInt(prodId)));
+		Product product = (Product) criteria.uniqueResult();
+		trans.commit();
+		return product;
 	}
 }
