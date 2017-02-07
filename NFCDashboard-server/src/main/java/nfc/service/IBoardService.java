347be0415e507043222e119815a257d8c03d96c9
TUANNGUYEN <nguyenhoangtuanht@gmai.com>
2017-02-07 17:01:50 +0700

    board

--- NFCDashboard-server/src/main/java/nfc/service/IBoardService.java
+++ NFCDashboard-server/src/main/java/nfc/service/IBoardService.java
@@ -1,7 +1,12 @@
 package nfc.service;
 
+import java.util.List;
+
 import nfc.model.Board;
+import nfc.model.Thread;
 
 public interface IBoardService {
 	boolean insertBoard(Board board);
+	List<Board> getListBoard(String userName);
+	List<Thread> getListThread(int board_id);
 }
--- NFCDashboard-server/src/main/java/nfc/serviceImpl/BoardService.java
+++ NFCDashboard-server/src/main/java/nfc/serviceImpl/BoardService.java
@@ -1,11 +1,17 @@
 package nfc.serviceImpl;
 
+import java.util.List;
+
+import org.hibernate.Criteria;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
+import org.hibernate.criterion.Restrictions;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import nfc.model.Board;
+import nfc.model.User;
+import nfc.model.Thread;
 import nfc.service.IBoardService;
 
 public class BoardService implements IBoardService{
@@ -32,6 +38,36 @@ public class BoardService implements IBoardService{
 			trans.rollback();
 			return false;
 		}
+	}
+	public List<Board> getListBoard(String userName)
+	{
+		Session session = this.sessionFactory.getCurrentSession();
+		Transaction trans = session.beginTransaction();
+		String userId = getUserIdfromUserName(userName, session);
+		Criteria criteria = session.createCriteria(Board.class);
+		criteria.add(Restrictions.eq("owner_id",userId));
+		List<Board> list = (List<Board>) criteria.list();
+		trans.commit();
+		return list;
+	}
+	public String getUserIdfromUserName(String userName, Session session)
+	{
+		Criteria criteria = session.createCriteria(User.class);
+		criteria.add(Restrictions.eq("user_name",userName));
+		List<User> list = (List<User>) criteria.list();		
+		String userId = list.get(0).getUser_id();
+		System.out.println(userId);
+		return userId; 
+	}
+	public List<Thread> getListThread(int board_id)
+	{
+		Session session = this.sessionFactory.getCurrentSession();
+		Transaction trans = session.beginTransaction();
+		Criteria criteria = session.createCriteria(Thread.class);
+		//criteria.add(Restrictions.eq("board_id",board_id));
+		List<Thread> list = (List<Thread>) criteria.list();
+		trans.commit();
+		return list;
 	}
 
 }
